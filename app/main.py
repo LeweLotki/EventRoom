5389ca509c0a842b06f7aab738e1cb20331ea839
LeweLotki <stefanczyk@gmail.com>
2024-08-02 19:42:18 +0200

    adding users resource

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.resources.users.routes import router as users_router
 from app.core.database import Base, engine
 from app.core.config import settings
 
@@ -11,6 +12,9 @@ app = FastAPI(
 
 # Create the database tables if they don't exist
 Base.metadata.create_all(bind=engine)
+
+# Include routers for different resources
+app.include_router(users_router, prefix="/users", tags=["users"])
 
 @app.get("/")
 def read_root():
