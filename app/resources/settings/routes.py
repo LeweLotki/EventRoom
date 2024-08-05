f27cae7e097fda9404a23a0106feedf0046345e3
LeweLotki <stefanczyk@gmail.com>
2024-08-05 14:22:25 +0200

    fix in setting update

--- app/resources/settings/routes.py
+++ app/resources/settings/routes.py
@@ -9,17 +9,13 @@ from app.resources.users.models import User
 
 router = APIRouter()
 
-@router.patch("/{user_id}/update", response_model=schemas.SettingResponse)
+@router.patch("/update", response_model=schemas.SettingResponse)
 def update_setting(
-    user_id: int,
     setting_update: schemas.SettingUpdate,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
-    if current_user.id != user_id:
-        raise HTTPException(status_code=403, detail="Not authorized to update this settings")
-    
-    updated_setting = crud.update_setting(db=db, user_id=user_id, setting_update=setting_update)
+    updated_setting = crud.update_setting(db=db, user_id=current_user.id, setting_update=setting_update)
     if updated_setting is None:
         raise HTTPException(status_code=404, detail="Setting not found")
     return updated_setting
