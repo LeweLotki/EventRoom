39eca1477671d35afd0403befeb87f2bacd52796
LeweLotki <stefanczyk@gmail.com>
2024-08-05 14:19:36 +0200

    fix in profiles update

--- app/resources/profiles/routes.py
+++ app/resources/profiles/routes.py
@@ -9,18 +9,13 @@ from app.resources.users.models import User
 
 router = APIRouter()
 
-@router.patch("/{user_id}/update", response_model=schemas.ProfileResponse)
+@router.patch("/update", response_model=schemas.ProfileResponse)
 def update_profile(
-    user_id: int,
     profile_update: schemas.ProfileUpdate,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
-    # Check if the current user matches the profile's user_id
-    if current_user.id != user_id:
-        raise HTTPException(status_code=403, detail="Not authorized to update this profile")
-    
-    updated_profile = crud.update_profile(db=db, user_id=user_id, profile_update=profile_update)
+    updated_profile = crud.update_profile(db=db, user_id=current_user.id, profile_update=profile_update)
     if updated_profile is None:
         raise HTTPException(status_code=404, detail="Profile not found")
     return updated_profile
